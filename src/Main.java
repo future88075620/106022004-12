ab292c44807471bb1df8ab7c0be055752084c4c8
future88075620 <future870914@gmail.com>
2017-09-30 15:17:45 +0800

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner in=new Scanner(System.in);
+        int a;
+        int b;
+        a=in.nextInt();
+        b=in.nextInt();
+        System.out.println(a/2.54);
+        System.out.println(b/0.454);
+    }
+}
